971356a119b9928d7d09a88d19cf799c20bcda2b
EC2 Default User <ec2-user@ip-172-16-180-242.ec2.internal>
2018-08-12 06:28:41 +0000

    finished string matching using fuzzywuzzy and saved pairs to efs

--- src/scripts.py
+++ src/scripts.py
@@ -57,3 +57,31 @@ def parallelize_dataframe(df, func):
     pool.close()
     pool.join()
     return df
+
+import re, math
+from collections import Counter
+
+WORD = re.compile(r'\w+')
+
+def get_cosine(vec1, vec2):
+    # print vec1, vec2
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+    return Counter(WORD.findall(text))
+
+def get_similarity(pair):
+    a = text_to_vector(pair[0].strip().lower())
+    b = text_to_vector(pair[1].strip().lower())
+
+    return get_cosine(a, b)
