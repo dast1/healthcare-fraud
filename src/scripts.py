1295bbbd79d7eb448c9649730c542755a66e6dc4
EC2 Default User <ec2-user@ip-172-16-180-242.ec2.internal>
2018-08-15 14:32:12 +0000

    cleaned up reduced by state and put it in src

--- src/scripts.py
+++ src/scripts.py
@@ -85,3 +85,37 @@ def get_similarity(pair):
     b = text_to_vector(pair[1].strip().lower())
 
     return get_cosine(a, b)
+def plot_multi(data, threshold_value, cols=None, spacing=.1, **kwargs):
+
+    from pandas import plotting
+
+    # Get default color style from pandas - can be changed to any other color list
+    if cols is None: cols = data.columns
+    if len(cols) == 0: return
+    colors = getattr(getattr(plotting, '_style'), '_get_standard_colors')(num_colors=len(cols))
+
+    # First axis
+    ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
+    ax.set_ylabel(ylabel=cols[0])
+    lines, labels = ax.get_legend_handles_labels()
+
+    for n in range(1, len(cols)):
+        # Multiple y-axes
+        ax_new = ax.twinx()
+        ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
+        data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)])
+        ax_new.set_ylabel(ylabel=cols[n])
+
+        # Proper legend position
+        line, label = ax_new.get_legend_handles_labels()
+        lines += line
+        labels += label
+    
+    # add State threshold
+    threshold_value = threshold_value
+    ax.axvline(threshold_value, color='r', linestyle='-', linewidth=.5)
+    
+    ax.legend(lines, labels, loc=0)
+    ax.grid(color='k', linestyle='--', linewidth=1)
+    ax.set_xlabel(xlabel='States')
+    return ax
